cc6bde2e2937b5370044d28154a15aa097c25cc4
Karina Maulitova <karinamaulitova@gmail.com>
2021-07-14 00:12:23 +0300

    fix: arrow

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import arrow from '../../images/arrow-icon.svg';
+
 
 function Portfolio() {
   return (
@@ -7,33 +7,30 @@ function Portfolio() {
       <h2 className='portfolio__title'>Портфолио</h2>
       <ul className='portfolio__links-list'>
         <li className='portfolio__link-item'>
-          <a href='https://how-to-learn-karina.netlify.app/' className='portfolio__link'>
+          <a
+            href='https://how-to-learn-karina.netlify.app/'
+            className='portfolio__link'
+          >
             <p className='portfolio__link-text'>Статичный сайт</p>
-            <img
-              className='portfolio__arrow-icon'
-              src={arrow}
-              alt='Стрелочка'
-            />
+            <p className='portfolio__link-text'>↗</p>
           </a>
         </li>
         <li className='portfolio__link-item'>
-          <a href='https://karinamaulitova.github.io/russian-travel/index.html' className='portfolio__link'>
+          <a
+            href='https://karinamaulitova.github.io/russian-travel/index.html'
+            className='portfolio__link'
+          >
             <p className='portfolio__link-text'>Адаптивный сайт</p>
-            <img
-              className='portfolio__arrow-icon'
-              src={arrow}
-              alt='Стрелочка'
-            />
+            <p className='portfolio__link-text'>↗</p>
           </a>
         </li>
         <li className='portfolio__link-item'>
-          <a href='https://karina.mesto.students.nomoredomains.club/' className='portfolio__link'>
+          <a
+            href='https://karina.mesto.students.nomoredomains.club/'
+            className='portfolio__link'
+          >
             <p className='portfolio__link-text'>Одностраничное приложение</p>
-            <img
-              className='portfolio__arrow-icon'
-              src={arrow}
-              alt='Стрелочка'
-            />
+            <p className='portfolio__link-text'>↗</p>
           </a>
         </li>
       </ul>
