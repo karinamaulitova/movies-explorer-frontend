c2c4714b58c7d51659fc1e9114bc83b5938ed30d
Karina Maulitova <karinamaulitova@gmail.com>
2021-07-18 17:00:31 +0300

    feat: add logo link to main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import logo from "../../images/logo.svg";
 
 function Header({children}) {
   return (
     <header className="header">
+      <Link className='header__link' to='/'>
       <img className="header__logo" src={logo} alt="Логотип" />
+      </Link>
       <div className='header__container'>
       {children}
       </div>
